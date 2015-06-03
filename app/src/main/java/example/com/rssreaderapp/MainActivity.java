069e458d65c02ac9811657f47cad4eedfba48159
ndnhuy <ndnhuy2504@gmail.com>
2015-06-03 23:07:31 +0700

    RSS Reader version 1.4 on github

--- app/src/main/java/example/com/rssreaderapp/MainActivity.java
+++ app/src/main/java/example/com/rssreaderapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-    // This is for version 1.4
+    // This is for version 1.4 on github
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
